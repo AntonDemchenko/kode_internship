4f697a66f50067a5befbd0cd18b3bff1cf774194
Anton Demchenko <d.flier@yandex.ru>
2019-04-06 19:28:28 +0200

    Add request function

--- src/base_app/integrations/speech_recognition.py
+++ src/base_app/integrations/speech_recognition.py
@@ -1,6 +1,32 @@
 import base64
+import requests
+from settings import SPEECH_RECOGNITION
+
+
+def make_request(audio: bytes) -> requests.Response:
+    try:
+        response = requests.post(
+            "https://speech.googleapis.com/v1/speech:recognize",
+            json={
+                "audio": {
+                    "content": audio,
+                    "config": {
+                        "encoding": SPEECH_RECOGNITION["AUDIO_ENCODING"],
+                        "languageCode": SPEECH_RECOGNITION["LANGUAGE_CODE"]
+                    }
+                }
+            },
+            params={"key": SPEECH_RECOGNITION["GOOGLE_API_KEY"]}
+        )
+    except requests.exceptions.RequestException:
+        return None
+    else:
+        return response
 
 
 def speech_to_text(audio: bytes) -> str:
     audio = base64.b64encode(audio)
+    audio = audio.decode("ascii")
+    response = make_request(audio)
+
     return "Hello"
