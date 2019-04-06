55a7c7645f4f15fab24687df83ea301c7f447d33
Anton Demchenko <d.flier@yandex.ru>
2019-04-06 14:19:56 +0200

    Make base view

--- src/base_app/views/speech_recognition.py
+++ src/base_app/views/speech_recognition.py
@@ -1,8 +1,11 @@
 from base_app.utils.json import Json
+from base_app.integrations.speech_recognition import speech_to_text
 
 
 class SpeechRecognition(Json):
     def post(self, request):
+        audio = request.FILES["audio"].read()
+        text = speech_to_text(audio)
         return {
-            "text": "Hello"
+            "text": text
         }
