78c9e5d5a2f8c32aaf4fb01a966cd3440052d6df
Anton Demchenko <d.flier@yandex.ru>
2019-04-07 12:23:33 +0200

    Handle audio file absence

--- src/base_app/views/speech_recognition.py
+++ src/base_app/views/speech_recognition.py
@@ -1,10 +1,15 @@
+import rmr
+
 from base_app.utils.json import Json
 from base_app.integrations.speech_recognition import speech_to_text
 
 
 class SpeechRecognition(Json):
     def post(self, request):
-        audio = request.FILES["audio"].read()
+        audio = request.FILES.get("audio")
+        if not audio:
+            raise rmr.ClientError("Please provide audio file.", code=400)
+        audio = audio.read()
         text = speech_to_text(audio)
         return {
             "text": text
