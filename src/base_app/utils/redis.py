90b8bafa94890a4a75dbedcf1017913b58c1eabb
ae <ae@appkode.ru>
2019-04-08 18:17:58 +0200

    redis connection

--- src/base_app/utils/redis.py
+++ src/base_app/utils/redis.py
@@ -3,6 +3,8 @@ from src.base_app.services import redis
 
 def save_pwd(login: str, pwd: str) -> bool:
     redis_connection = redis.connection()
+    if not redis_connection:
+        return False
     is_saved = redis_connection.set(login, pwd)
 
     return is_saved
@@ -10,6 +12,10 @@ def save_pwd(login: str, pwd: str) -> bool:
 
 def get_pwd(login: str) -> str:
     redis_connection = redis.connection()
+    if not redis_connection:
+        return ''
     pwd = redis_connection.get(login)
+    if not pwd:
+        return ''
 
     return pwd.decode()
