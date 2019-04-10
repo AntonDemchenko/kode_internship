f7040c9a73f9a6e2aa7fecb0447d11bddaa07b52
Anton Demchenko <d.flier@yandex.ru>
2019-04-10 17:39:07 +0200

    Make db models

--- src/base_app/models/user.py
+++ src/base_app/models/user.py
@@ -7,5 +7,6 @@ class User(models.Model):
     user_id = models.CharField(max_length=128, default=uuid.uuid4, primary_key=True)
     first_name = models.CharField(max_length=128, default='')
     last_name = models.CharField(max_length=128, default='')
-    username = models.CharField(max_length=128, default='')
+    username = models.CharField(max_length=128, default='', unique=True)
     email = models.CharField(max_length=128, default='')
+    password = models.CharField(max_length=128, default='')
