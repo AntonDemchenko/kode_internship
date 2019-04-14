44378a135672a547a79e9b3ac5e72c6fe55f1caf
Anton Demchenko <d.flier@yandex.ru>
2019-04-14 18:03:44 +0200

    Hide password

--- src/base_app/serializers/user.py
+++ src/base_app/serializers/user.py
@@ -8,6 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         raw_password = validated_data.get("password")
