e376099877707b09909ad44dbb3f9cb00f5de1fe
Anton Demchenko <d.flier@yandex.ru>
2019-04-16 15:49:26 +0200

    Add getters in subscription model

--- src/base_app/models/subscription.py
+++ src/base_app/models/subscription.py
@@ -7,5 +7,13 @@ from .user import User
 
 class Subscription(models.Model):
     subs_id = models.CharField(max_length=128, default=uuid.uuid4, primary_key=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
-    target = models.ForeignKey(User, on_delete=models.CASCADE, related_name='target')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='outgoing_subs')
+    target = models.ForeignKey(User, on_delete=models.CASCADE, related_name='incoming_subs')
+
+    @classmethod
+    def get_outgoing_subs(cls, user_id):
+        return cls.objects.filter(owner__user_id=user_id).all()
+
+    @classmethod
+    def get_incoming_subs(cls, user_id):
+        return cls.objects.filter(target__user_id=user_id).all()
