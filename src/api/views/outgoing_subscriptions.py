5b981ee3ea5b3ecdb141acc37dbfa8c87439de69
Anton Demchenko <d.flier@yandex.ru>
2019-04-16 17:18:49 +0200

    Add method creating outgoing subscription

--- src/api/views/outgoing_subscriptions.py
+++ src/api/views/outgoing_subscriptions.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.permissions import BasePermission, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -20,3 +21,11 @@ class OutgoingSubscriptionList(APIView):
         subscriptions = Subscription.get_outgoing_subs(pk)
         serializer = SubscriptionSerializer(subscriptions, many=True)
         return Response(serializer.data)
+
+    def post(self, request, pk):
+        request.data['owner'] = pk
+        serializer = SubscriptionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
