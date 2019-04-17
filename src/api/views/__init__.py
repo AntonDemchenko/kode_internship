011778aaf9eaeb343633652f972a3e777a716d35
Anton Demchenko <d.flier@yandex.ru>
2019-04-17 13:26:48 +0200

    Rename view file

--- src/api/views/__init__.py
+++ src/api/views/__init__.py
@@ -1,6 +1,6 @@
 from .auth import PublicKeyView
 from .credentials import CredentialsView
-from .subscriptions import OutgoingSubscriptionList
+from .subscription import OutgoingSubscriptionList
 from .user import UserDetail, UserList, UserSearch
 
 __all__ = [
