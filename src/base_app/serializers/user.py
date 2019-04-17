2c9fda78810e3cc6953c85f3895371d9eb526f40
Anton Demchenko <d.flier@yandex.ru>
2019-04-17 10:06:10 +0200

    Reduce number of fields of user serializer

--- src/base_app/serializers/user.py
+++ src/base_app/serializers/user.py
@@ -6,8 +6,11 @@ from base_app.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('user_id', 'username', 'email', 'password')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'user_id': {'read_only': True}
+        }
 
     def create(self, validated_data):
         user = User.create_user(**validated_data)
