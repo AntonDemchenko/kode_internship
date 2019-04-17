ce9abed0345e14cf5474a2e2ab3a155d91d25112
Anton Demchenko <d.flier@yandex.ru>
2019-04-17 14:27:35 +0200

    Add templates for emails

--- src/base_app/models/subscription.py
+++ src/base_app/models/subscription.py
@@ -4,9 +4,13 @@ from django.conf import settings
 from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from django.template import loader
 from django_q.tasks import async_task
 
 from .user import User
+
+NEW_SUBSCRIBER_NOTICE_TITLE_PATH = 'new_subscriber_notice_title.txt'
+NEW_SUBSCRIBER_NOTICE = 'new_subscriber_notice.txt'
 
 
 class Subscription(models.Model):
@@ -30,12 +34,24 @@ class Subscription(models.Model):
         return cls.objects.get(owner__user_id=owner_id, target__user_id=target_id)
 
 
-@receiver(pre_save, sender=Subscription)
-def subscription_added(sender, instance, **kwargs):
+def render_template(template_path: str, context: dict) -> str:
+    template = loader.get_template(template_path)
+    return template.render(context)
+
+
+def send_email(title: str, message: str, to: str) -> None:
     async_task(
         'django.core.mail.send_mail',
-        'Subscription',
-        'User {} is subscribed on you'.format(instance.owner.username),
+        title,
+        message,
         from_email=settings.EMAIL_HOST_USER,
-        recipient_list=[instance.target.email]
+        recipient_list=[to]
     )
+
+
+@receiver(pre_save, sender=Subscription)
+def subscription_added(sender, instance, **kwargs):
+    context = dict(subscriber=instance.owner)
+    notice_title = render_template(NEW_SUBSCRIBER_NOTICE_TITLE_PATH, context)
+    notice = render_template(NEW_SUBSCRIBER_NOTICE, context)
+    send_email(notice_title, notice, instance.target.email)
