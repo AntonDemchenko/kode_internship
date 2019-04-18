112acb1fc69267c61e1b37223991e07cdb77f590
Anton Demchenko <d.flier@yandex.ru>
2019-04-18 21:50:43 +0200

    Change field of audio to FileField

--- src/base_app/models/pitt.py
+++ src/base_app/models/pitt.py
@@ -5,9 +5,13 @@ from django.db import models
 from .user import User
 
 
+def user_directory_path(instance, filename):
+    return 'user_{}/{}'.format(instance.user.id, filename)
+
+
 class Pitt(models.Model):
     pitt_id = models.CharField(max_length=128, default=uuid.uuid4, primary_key=True)
-    audio = models.BinaryField()
+    audio = models.FileField(upload_to=user_directory_path)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
