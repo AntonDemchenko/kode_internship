e46d7b8743499a9260a9ea69787f35f41449c2d2
Anton Demchenko <d.flier@yandex.ru>
2019-04-19 16:35:29 +0200

    Add pitt serializer

--- src/base_app/serializers/__init__.py
+++ src/base_app/serializers/__init__.py
@@ -1,7 +1,9 @@
+from .pitt import PittSerializer
 from .subscription import SubscriptionSerializer
 from .user import UserSerializer
 
 __all__ = [
+    'PittSerializer',
     'SubscriptionSerializer',
     'UserSerializer'
 ]
---
+++ src/base_app/serializers/pitt.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from base_app.models import Pitt
+
+
+class PittSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pitt
+        fields = ('pitt_id', 'text', 'audio', 'created_at', 'user')
+        extra_kwargs = {
+            'pitt_id': {'read_only': True},
+            'created_id': {'created_at': True}
+        }
