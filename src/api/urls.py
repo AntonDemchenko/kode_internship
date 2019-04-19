4ab3606c2af8a854ac31a4a5c8c88b004c0b5176
Anton Demchenko <d.flier@yandex.ru>
2019-04-19 17:28:02 +0200

    Add pitt view to urlpatterns

--- src/api/urls.py
+++ src/api/urls.py
@@ -4,13 +4,14 @@ from rest_framework_simplejwt.views import (
 )
 
 from . import views
-
 urlpatterns = [
     path('users', views.UserList.as_view(), name='user list'),
     path('users/search', views.UserSearch.as_view(), name='user search'),
     path('users/<str:pk>', views.UserDetail.as_view(), name='user details'),
+    path('users/<str:user_id>/pitts', views.PittList.as_view(), name='pitt list'),
     path('users/<str:user_id>/subscriptions', views.OutgoingSubscriptionList.as_view(), name='subscription list'),
     path('creds', views.CredentialsView.as_view(), name='creds'),
     path('login', TokenObtainPairView.as_view(), name='login'),
     path('pubkey', views.PublicKeyView.as_view(), name='public key'),
 ]
+
--- src/api/views/__init__.py
+++ src/api/views/__init__.py
@@ -1,13 +1,15 @@
 from .auth import PublicKeyView
 from .credentials import CredentialsView
+from .pitt import PittList
 from .subscription import OutgoingSubscriptionList
 from .user import UserDetail, UserList, UserSearch
 
 __all__ = [
+    'CredentialsView',
+    'OutgoingSubscriptionList',
+    'PittList',
+    'PublicKeyView',
     'UserList',
     'UserDetail',
     'UserSearch',
-    'OutgoingSubscriptionList',
-    'CredentialsView',
-    'PublicKeyView'
 ]
