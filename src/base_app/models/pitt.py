c4e5e47ba7ffa5623c44072dd6ff2b6de2d310a6
Anton Demchenko <d.flier@yandex.ru>
2019-04-20 22:09:13 +0200

    Use charfield for text of pitt

--- src/base_app/models/pitt.py
+++ src/base_app/models/pitt.py
@@ -12,7 +12,7 @@ def user_directory_path(instance, filename):
 class Pitt(models.Model):
     pitt_id = models.CharField(max_length=128, default=uuid.uuid4, primary_key=True)
     audio = models.FileField(upload_to=user_directory_path)
-    text = models.TextField()
+    text = models.CharField(max_length=256)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
